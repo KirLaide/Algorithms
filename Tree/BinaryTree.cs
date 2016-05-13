1e15f7fddfc334a48aa399daeb3e87cb956ea6e3
KirLaide <yurii.disnenko@gmail.com>
2016-05-13 11:28:31 +0300

    Height of Tree

--- Tree/BinaryTree.cs
+++ Tree/BinaryTree.cs
@@ -49,6 +49,13 @@ namespace BinaryTree
             return root;
         }
 
+        // tree Height O(n)
+        public static int treeHeight(Node n)
+        {
+            if(n == null) return 0;
+            return 1 + Math.max( treeHeight( n.getLeft()), treeHeight(n.getRight()));
+        }
+
     }
     
 }
