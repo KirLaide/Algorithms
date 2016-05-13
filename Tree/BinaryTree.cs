40d58e2fe9fb6fa4f879cd5e546748a81883e555
KirLaide <yurii.disnenko@gmail.com>
2016-05-13 11:11:39 +0300

    BinaryTree

---
+++ Tree/BinaryTree.cs
@@ -0,0 +1,54 @@
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        private Node left;
+        private Node right;
+        private int value;
+
+        public Node(Node left, Node right, int value)
+        {
+            this.left = left;
+            this.right = right;
+            this.value = value;
+        }
+
+        public Node getLeft()
+        {
+            return left;
+        }
+
+        public Node getRight()
+        {
+            return right;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+
+        //find O(log(n))
+        public Node findNode(Node root, int value)
+        {
+            while (root != null)
+            {
+                int currval = root.getValue();
+                if(currval == value) break;
+                if (currval < value)
+                {
+                    root = root.getRight();
+                }
+                else
+                {
+                    //currval > value
+                    root = root.getLeft();
+                }
+            }
+            return root;
+        }
+
+    }
+    
+}
