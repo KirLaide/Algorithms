6f9a2353dd54e5a381c86f36b9cb6d56572f6453
KirLaide <yurii.disnenko@gmail.com>
2016-07-26 10:14:50 +0300

    Shell sorting Lafore

--- CodeFromBookLaforeRobert/Program.cs
+++ CodeFromBookLaforeRobert/Program.cs
@@ -97,6 +97,64 @@ class DArray
     }
 }
 #endregion
+
+#region ShellSort
+
+class ArraySh
+{
+    private long[] theArray;
+    private int nElems;
+
+    public ArraySh(int max)
+    {
+        theArray = new long[max];
+        nElems = 0;
+    }
+
+    public void insert(long value)
+    {
+        theArray[nElems] = value;
+        nElems++;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("A=");
+        for (int j = 0; j < nElems; j++)
+        {
+            Console.WriteLine(theArray[j] + " ");
+        }
+        Console.WriteLine("");
+    }
+
+    public void shellSort()
+    {
+        int inner, outer;
+        long temp;
+        int h = 1;
+        while (h <= nElems/3)
+            h = h*3 + 1;
+        while (h > 0)
+        {
+            for (outer = h; outer < nElems; outer++)
+            {
+                temp = theArray[outer];
+                inner = outer;
+
+                while (inner > h - 1 && theArray[inner-h] >= temp)
+                {
+                    theArray[inner] = theArray[inner - h];
+                    inner -= h;
+                }
+                theArray[inner] = temp;
+            }
+            h = (h - 1)/3;
+        }
+    }
+}
+
+
+#endregion
 class Program
 {
     public static void insert(int[] array, int value, int startfrom)
@@ -238,7 +296,22 @@ class Program
         */
         /*EndMergeSort*/
         #endregion
-        Console.ReadLine();
+
+        #region ShellMain
+            int maxSize = 10;
+            ArraySh arr;
+            Random rnd = new Random();
+            arr = new ArraySh(maxSize);
+           for (int j = 0; j < maxSize; j++)
+           {
+                long n = (int)( rnd.Next(1,600) * 99);
+                arr.insert(n);
+           }
+           arr.display();
+           arr.shellSort();
+           arr.display();
+       #endregion
+            Console.ReadLine();
     }
 }
    
